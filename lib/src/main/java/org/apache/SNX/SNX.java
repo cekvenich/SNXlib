b0aeb0937af34357f252066bfc5ed7bc535dd130
Vic <vic@eml.cc>
2020-01-21 11:42:28 -0500

    kick

--- lib/src/main/java/org/apache/SNX/SNX.java
+++ lib/src/main/java/org/apache/SNX/SNX.java
@@ -6,7 +6,7 @@ public class SNX {
 	 * Just print out some version / info
 	 */
 	public String getSNX() {
-		String ver = "V: 0.0.114";
+		String ver = "V: 0.0.01";
 		System.out.println(ver);
 		return ver;
 	}
